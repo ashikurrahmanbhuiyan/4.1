dadc77aee3c99c292a3854a2d49f47882dc184b3
ashik <s2010376107@ru.ac.bd>
2025-05-02 10:32:08 +0600

    init

---
+++ parallel processing/previous/CUDA/Phonebook Search.cu
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+#include <cuda.h>
+
+using namespace std;
+
+struct Contact
+{
+    char name[256];
+    char phone_number[256];
+};
+
+__device__ bool check(char* name, char *search_name, int length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        if (name [i] != search_name[i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+__global__ void searchPhonebook(Contact* phonebook, int size, char* search_name, int length)
+{
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (index < size)
+    {
+        if (check(phonebook[index].name, search_name, length))
+        {
+            printf("Name: %s, Phone: %s\n", phonebook[index].name, phonebook[index].phone_number);
+        }
+    }
+}
+
+int main()
+{
+    vector<string> file_names = {"phonebook1.txt", "phonebook2.txt"};
+    vector<Contact> phonebook;
+
+    for (auto file_name: file_names)
+    {
+        ifstream file(file_name);
+        Contact contact;
+        while (file >> contact.name >> contact.phone_number)
+        {
+            phonebook.push_back(contact);
+        }
+        file.close();
+    }
+
+    int size = phonebook.size();
+
+    cudaEvent_t start; //... Start time
+    cudaEventCreate(&start); 
+
+    cudaEvent_t end; //... End time
+    cudaEventCreate(&end);
+
+    Contact* device_phonebook;
+    cudaMalloc((void **)&device_phonebook, sizeof(Contact) * size);
+
+    cudaMemcpy(device_phonebook, phonebook.data(), sizeof(Contact) * size, cudaMemcpyHostToDevice);
+
+    string search_name = "Sophie";
+    int name_length = search_name.size() + 1;
+    char* device_search_name;
+    cudaMalloc((void **)&device_search_name, name_length);
+
+    cudaMemcpy(device_search_name, search_name.c_str(), name_length, cudaMemcpyHostToDevice);
+
+    int blockSize = 256;
+    int gridSize = (size + blockSize - 1) / blockSize;
+
+    cudaEventRecord(start);
+
+    searchPhonebook<<<gridSize, blockSize>>>(device_phonebook, size, device_search_name, name_length);
+
+    cudaEventRecord(end);
+    cudaEventSynchronize(end);
+
+    float time_taken = 0;
+    cudaEventElapsedTime(&time_taken, start, end);
+    cout << "\nTime taken to execute: " << time_taken << " ms\n\n";
+    
+    return 0;
+}
---
+++ parallel processing/previous/CUDA/Readme.md
@@ -0,0 +1,11 @@
+# How to run CUDA on Google Colab
+- **To install CUDA plugin**
+<pre>
+<b>!pip install git+https://github.com/andreinechaev/nvcc4jupyter.git</b>
+<b>%load_ext nvcc_plugin</b>
+</pre>
+- **Code format**
+<pre>
+<b>%%cu</b>
+<b>Code starts here...</b>
+</pre>
